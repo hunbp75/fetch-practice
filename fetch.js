886d16be993065726358b15bdab8ea8e349e6365
hunbp75 <hunbp75@gmail.com>
2021-12-15 16:00:12 +0100

    go

--- fetch.js
+++ fetch.js
@@ -1,33 +1,35 @@
-let input = document.querySelector(".input_text");
-let main = document.querySelector("#name");
-let temporary = document.querySelector(".temporary");
-let skyCondition = document.querySelector(".skyCondition");
-let humidity = document.querySelector(".humidity");
-let button = document.querySelector(".submit");
+function createDom() {
+  let input = document.querySelector(".input_text");
+  let main = document.querySelector("#name");
+  let temporary = document.querySelector(".temporary");
+  let skyCondition = document.querySelector(".skyCondition");
+  let humidity = document.querySelector(".humidity");
+  let button = document.querySelector(".submit");
 
-function createDom() {}
+  button.addEventListener("click", function (name) {
+    let search = new URLSearchParams();
+    search.append("key", "6664ed8bd58945d3b8a194201211412");
+    search.append("q", input.value);
+    let fetchURL = `https://api.weatherapi.com/v1/current.json?${search.toString()}`;
 
-button.addEventListener("click", function (name) {
-  let search = new URLSearchParams();
-  search.append("key", "6664ed8bd58945d3b8a194201211412");
-  search.append("q", input.value);
-  let fetchURL = `https://api.weatherapi.com/v1/current.json?${search.toString()}`;
+    fetch(fetchURL)
+      .then((response) => response.json())
+      .then((data) => {
+        let nameValue = data.location.name;
+        let temporaryValue = data.current.temp_c;
+        let skyConditionValue = data.current.condition.text;
+        let humidityValue = data.current.humidity;
 
-  fetch(fetchURL)
-    .then((response) => response.json())
-    .then((data) => {
-      let nameValue = data.location.name;
-      let temporaryValue = data.current.temp_c;
-      let skyConditionValue = data.current.condition.text;
-      let humidityValue = data.current.humidity;
+        main.innerHTML = nameValue;
+        temporary.innerHTML = "Temporary: " + temporaryValue;
+        skyCondition.innerHTML = "Sky condition: " + skyConditionValue;
+        humidity.innerHTML = "Humidity: " + humidityValue;
 
-      main.innerHTML = nameValue;
-      temporary.innerHTML = "Temporary: " + temporaryValue;
-      skyCondition.innerHTML = "Sky condition: " + skyConditionValue;
-      humidity.innerHTML = "Humidity: " + humidityValue;
+        input.value = "";
+      })
 
-      input.value = "";
-    })
+      .catch((err) => alert("Wrong city name!"));
+  });
+}
 
-    .catch((err) => alert("Wrong city name!"));
-});
+window.addEventListener("load", createDom);
--- index.js
+++ index.js
@@ -1,5 +1,63 @@
-function createDom() {}
+function createDom() {
+  let appContainer = document.createElement("div");
+  appContainer.id = "app-container";
 
-function main() {}
+  let input = document.createElement("input");
+  input.id = "input";
+  input.type = "text";
+
+  let city = document.createElement("h1");
+  city.id = "town";
+
+  let temp = document.createElement("h1");
+  temp.id = "temporary";
+
+  let humidity = document.createElement("h1");
+  humidity.id = "humidity";
+
+  let clouds = document.createElement("h1");
+  clouds.id = "clouds";
+
+  $("#root").append(appContainer);
+  appContainer.append(input, city, temp, humidity, clouds);
+}
+
+function fetchApi() {
+  let input = document.querySelector("#input");
+  let cityName = document.querySelector("#town");
+  let temporary = document.querySelector("#temporary");
+  let skyCondition = document.querySelector("#clouds");
+  let humidity = document.querySelector("#humidity");
+
+  input.addEventListener("change", function (name) {
+    let search = new URLSearchParams();
+    search.append("key", "6664ed8bd58945d3b8a194201211412");
+    search.append("q", input.value);
+    let fetchURL = `https://api.weatherapi.com/v1/current.json?${search.toString()}`;
+
+    fetch(fetchURL)
+      .then((response) => response.json())
+      .then((data) => {
+        let nameValue = data.location.name;
+        let temporaryValue = data.current.temp_c;
+        let skyConditionValue = data.current.condition.text;
+        let humidityValue = data.current.humidity;
+
+        cityName.innerHTML = nameValue;
+        temporary.innerHTML = "Temporary: " + temporaryValue;
+        skyCondition.innerHTML = "Sky condition: " + skyConditionValue;
+        humidity.innerHTML = "Humidity: " + humidityValue;
+
+        input.value = "";
+      })
+
+      .catch((err) => alert("Wrong city name!"));
+  });
+}
+
+function main() {
+  createDom();
+  fetchApi();
+}
 
 window.addEventListener("load", main);
